75392dbe830182cf4c9d56e35b000044d903c137
elmazemuratii <elmaze.murati@student.uni-pr.edu>
2026-04-17 22:34:31 +0200

    Shtova alias për host dhe port

--- config.php
+++ config.php
@@ -4,6 +4,10 @@ $tcpHost = "127.0.0.1";
 $tcpPort = 9000;
 $max_clients = 4;
 
+//// konfigurimi i lidhjes me serverin 
+$host = $tcpHost;
+$port = $tcpPort;
+
 // HTTP
 $httpHost = "127.0.0.1";
 $httpPort = 9090;
